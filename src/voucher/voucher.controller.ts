12504f7ee0e7b6b6dee32c1514da2afe426ffdc1
Nanra Sukedy <sukedynanra@gmail.com>
2022-05-19 13:31:30 +0700

    :hammer: Add JWT Guards for Voucher Controller :hammer:

--- src/voucher/voucher.controller.ts
+++ src/voucher/voucher.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
+import { JwtOtpGuard } from "src/auth/jwt-otp.guard";
 import { CreateVoucherDto } from "./dto/create-voucher.dto";
 import { VoucherService } from "./voucher.service";
 
@@ -9,16 +10,19 @@ export class VoucherController{
         private readonly voucherService: VoucherService
     ) {}
 
+    @UseGuards(JwtOtpGuard)
     @Post('create')
     async createUserPoint(@Body() payload: CreateVoucherDto) {
         return this.voucherService.create(payload);
     }
 
+    @UseGuards(JwtOtpGuard)
     @Get()
     async getVouchers() {
         return this.voucherService.getVouchers();
     }
 
+    @UseGuards(JwtOtpGuard)
     @Get('detail')
     async getVoucherDetail(@Query() {idVoucher}) {
         return this.voucherService.findById(idVoucher);
